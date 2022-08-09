36289b1d408c3c8b5f49b74d3835c806b1aee144
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-08-09 12:14:30 -0600

    unsuccessfully fix It 4

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -33,12 +33,23 @@ describe('URL Shortener', () => {
   });
 
   it("should display the new shortened URL when the user fills out and submits the form", () => {
+    
     cy.get(".url").should("have.length", 1)
     cy.get("input").first().type("Howdy")
     cy.get("input").last().type("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
     cy.get(".button").click()
     cy.intercept("http://localhost:3001/api/v1/urls", { fixture: "posted-urls.json" })
-    cy.get(".url").should("have.length", 2)
+    cy.intercept("POST", "http://localhost:3001/api/v1/urls", {
+          statusCode: 201,
+          body: {
+            id:2,
+            long_url: "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+            short_url: "http://localhost:3001/useshorturl/1",
+            title: "Howdy"
+          }
+      })
   })
+
+
 
 })
